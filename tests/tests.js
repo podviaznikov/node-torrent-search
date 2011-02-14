ccf573a98018db9c3d0b87d0113320d189d88b21
Anton Podviaznikov <podviaznikov@gmail.com>
2011-02-14 16:19:49 +0000

    fixed tests for the node 0.4

--- tests/tests.js
+++ tests/tests.js
@@ -1,16 +1,17 @@
-var torrent = require('../torrent.js').create();
-exports.testFakeItem = function(assert)
+var torrent = require('../torrent.js').create(),
+    assert = require('assert');
+exports.testFakeItem = function()
 {
     torrent.find({ihq:'dssadjaskdjaskjdksajdkasjdsajdkasdjaskdjkas'},function(data)
     {
         assert.equal(0,data.total_results);
     });
 };
-exports.testFindFirstMovieItem = function(assert)
+exports.testFindFirstMovieItem = function()
 {
     torrent.find({ihq:'Inception',rows:1},function(data)
     {
-        assert.length(data.items.list,1);
+        assert.equal(data.items.list.length,1);
         var movieItem = data.items.list[0];
         assert.match(movieItem.title,/Inception/)
     });
